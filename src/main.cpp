fa1836b27e03a146eb774beb9b9d2cadd94bf2f7
Richard Riley <rileyrg@gmail.com>
2019-09-12 11:37:56 +0200

    added pdf

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,18 @@
 #include <Arduino.h>
 
 
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
 void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
+  sensorValue = analogRead(sensorPin);
+  String s = "Sensor Value (0-1023): ";
+  Serial.println(s+sensorValue);
+  delay(1000);
 }
